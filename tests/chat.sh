5eadfd961e3ab6b89a629d7818d72a1aac1bacbf
zTgx <747674262@qq.com>
2024-12-24 08:20:32 +0800

    feat: rm tests

--- tests/chat.sh
+++
@@ -1,6 +0,0 @@
-curl -X POST http://localhost:8080/api/chat \
-  -H "Content-Type: application/json" \
-  -d '{
-    "character_id": "char123",
-    "message": "Hello, this is a test message!"
-  }'
